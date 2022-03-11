7d8c531a5ba5963149bf5b29c937141208f69b60
Emmakki <emmivdc@gmail.com>
2022-03-11 10:02:19 +0100

    ajout doc avec mongoose

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 //connexion à la base de données avant le lancement du serveur
 const MongoClient = require('mongodb').MongoClient;
 
@@ -9,14 +10,36 @@ MongoClient.connect(url, function(err, client) {
   const db = client.db(dbName);
   client.close();
 });
+*/
 
 //connexion a mongoose (structurer la BD) avant lancement du serveur
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/PWEB_echec', {useNewUrlParser: true, useUnifiedTopology: true});
 
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("connecté à Mongoose")
+  console.log("connecté à Mongoose");
 });
+
+//Création Schéma enregistrement d'une partie
+const Schema = mongoose.Schema;
+const partiSchema = new Schema({
+    joueur:  String,
+    party: JSON,
+    temps_départ:   String,
+  });
+//Création du model
+const Partie = mongoose.model('parties', partiSchema)
+
+//Création d'un enregistrement
+const testParti = new Partie ({joueur : "Emma"});
+
+//Sauvegarder dans la DB
+testParti.save(function (err) {
+  if (err) {throw err;}
+  console.log('Parti enregistrée');
+  //Déconnexion
+  mongoose.connection.close();
+})
